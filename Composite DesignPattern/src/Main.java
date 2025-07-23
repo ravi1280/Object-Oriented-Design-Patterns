51e2cafae687b5932615002ba48a70f59874fd68
ravi1280 <ravishkaindrajith9.9@gmail.com>
2025-07-23 11:30:03 +0530

    add files

---
+++ Composite DesignPattern/src/Main.java
@@ -0,0 +1,16 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        File file1 = new File("Resume.pdf");
+        File file2 = new File("photo.png");
+        File file3 = new File("Budget.jpg");
+
+
+
+    }
+}
+
