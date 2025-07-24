99fce03b0347066f114b649e24350b4504335b82
ravi1280 <ravishkaindrajith9.9@gmail.com>
2025-07-24 19:39:01 +0530

    Update Decorator.java

--- Decorator Design Pattern/src/Decorator.java
+++ Decorator Design Pattern/src/Decorator.java
@@ -1,4 +1,63 @@
 
+interface Coffee{
+      double cost();
+      String description();
+}
+
+class simpleCoffee implements Coffee{
+
+    @Override
+    public double cost() {
+        return 500;
+    }
+
+    @Override
+    public String description() {
+        return "Simple Coffee";
+    }
+}
+
+abstract class coffeeDecorator implements Coffee {
+    protected Coffee coffee;
+
+    public coffeeDecorator(Coffee coffee){
+        this.coffee = coffee;
+    }
+}
+
+class MilkDecorator extends coffeeDecorator{
+    public MilkDecorator(Coffee coffee){
+        super(coffee);
+    }
+
+    @Override
+    public double cost() {
+        return coffee.cost()+100;
+    }
+
+    @Override
+    public String description() {
+        return coffee.description()+",Milk";
+    }
+}
+
+class SugarDecorator extends coffeeDecorator{
+    public SugarDecorator(Coffee coffee){
+        super(coffee);
+    }
+
+    @Override
+    public double cost() {
+        return coffee.cost()+50;
+    }
+
+    @Override
+    public String description() {
+        return coffee.description()+",Sugar";
+    }
+}
+
+
 public class Decorator {
     public static void main(String[] args) {
 
